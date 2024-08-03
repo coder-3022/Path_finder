5503f6f162fc04e260b43c7bec3ef0ac52df6da8
Sandeep kumar Gupta <coder5472@gmail.com>
2024-08-03 23:52:53 +0530

    final code
    frontend application

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import PathFinding from './PathFinding/PathFinding'
+function App() {
+  return (
+    <div>
+      <PathFinding/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer class="footer bg-dark py-3 mt-auto">
+      <div class="container text-center">
+        <span class=" text-white">
+          Made by &copy; Sandeep Kumar Gupta{" "}
+          <a href="https://github.com/coder-3022">
+            <FaGithub />
+          </a>{" "}
+          <a href="https://www.linkedin.com/in/">
+            <FaLinkedin />
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
